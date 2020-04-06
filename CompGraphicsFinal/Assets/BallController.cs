098f02ecf41b0b7cd438038bf8559885b3540bd4
harisrocker1 <harisrocker1@gmail.com>
2020-04-06 16:32:15 -0400

    Ball controler scripts added, Zoom out zoom in with scroll.

---
+++ CompGraphicsFinal/Assets/BallController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private bool onGround;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        onGround = true;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onGround)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity = new Vector3(0, 15, 0);
+                onGround = false;
+            }
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+
+        float moveHorizntal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 mov = new Vector3(moveHorizntal, 0, moveVertical);
+
+        rb.AddForce(mov * speed);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+    }
+}
---
+++ CompGraphicsFinal/Assets/CameraFinal.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFinal : MonoBehaviour
+{
+
+    public GameObject player;
+    public float MouseLookSens;
+    private Vector3 offset;
+    public float movespeed;
+
+    private float X = 0;
+    private float Y = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            offset = offset *1.1f;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f) // forward
+        {
+            offset = offset / 1.1f;
+        }
+       /* if (Input.GetMouseButton(1))
+        {
+            X = X + Input.GetAxis("Mouse X") * MouseLookSens;
+            Y = Y + Input.GetAxis("Mouse Y") * MouseLookSens;
+            Y = Mathf.Clamp(Y, -90, 90);
+
+
+            transform.localRotation = Quaternion.AngleAxis(X, Vector3.up);
+            transform.localRotation = transform.localRotation * Quaternion.AngleAxis(Y, Vector3.left);
+       
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * MouseLookSens, Vector3.up) * offset;
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform.position);
+        */
+    }
+
+    void LateUpdate()
+    {
+        
+    }
+    // Update is called once per frame , Guranted to run after all items have been processed
+
+}
