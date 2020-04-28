ef9cfc377550b717bdeff55d438311db46e56d05
harisrocker1 <harisrocker1@gmail.com>
2020-04-27 20:33:10 -0400

    Addeing Sun feature

---
+++ Assets/Scripts/SunRevolver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunRevolver : MonoBehaviour
+{
+    float timeCounter = 0;
+
+    float speed;
+    float width;
+    float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0.5f;
+        width= 50;
+        height = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime * speed;
+
+        float x = Mathf.Cos(timeCounter) *width;
+        float y = Mathf.Sin(timeCounter) * height;
+        float z = 0;
+
+        transform.localEulerAngles = new Vector3(x, y, z);
+
+    }
+}
