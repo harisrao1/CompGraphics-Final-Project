f2cc9fd640cf377b53302ddfafee2e2b47a5e38d
Justin <inotjustin@gmail.com>
2020-05-06 09:31:45 -0400

    main menu
    ball main menu

---
+++ Assets/Scripts/SwitchSceneTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchSceneTrigger : MonoBehaviour
+{
+    public bool active = false;
+    void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey("e"))
+        {
+            if (gameObject.name == "sub_area_1")
+            {
+                SceneManager.LoadScene("BallScene");
+            }
+            else if (gameObject.name == "sub_area_2")
+            {
+                SceneManager.LoadScene("CineMachineScene");
+            }
+        }
+    }
+}
